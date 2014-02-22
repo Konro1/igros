e8f9aefee48ecb1b56bd131d1617d5272923ceae
ym <ym@ym.(none)>
2014-02-22 14:37:09 +0200

    init

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://igros.mls/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://igros.mls/');
+
+// DIR
+define('DIR_APPLICATION', '/srv/www/igros.mls/catalog/');
+define('DIR_SYSTEM', '/srv/www/igros.mls/system/');
+define('DIR_DATABASE', '/srv/www/igros.mls/system/database/');
+define('DIR_LANGUAGE', '/srv/www/igros.mls/catalog/language/');
+define('DIR_TEMPLATE', '/srv/www/igros.mls/catalog/view/theme/');
+define('DIR_CONFIG', '/srv/www/igros.mls/system/config/');
+define('DIR_IMAGE', '/srv/www/igros.mls/image/');
+define('DIR_CACHE', '/srv/www/igros.mls/system/cache/');
+define('DIR_DOWNLOAD', '/srv/www/igros.mls/download/');
+define('DIR_LOGS', '/srv/www/igros.mls/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'password');
+define('DB_DATABASE', 'oc_igros');
+define('DB_PREFIX', 'oc_');
+?>
